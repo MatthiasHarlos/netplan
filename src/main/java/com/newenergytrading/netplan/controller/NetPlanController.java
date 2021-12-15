3e8e9cc4559939c632272de12aed2b3ff7c02e92
Your Name <you@example.com>
2021-12-15 11:24:00 +0100

    bonus changing knotInputForms changeSavedKnotInputForm() and deleteOldNetPlan()

--- src/main/java/com/newenergytrading/netplan/controller/NetPlanController.java
+++ src/main/java/com/newenergytrading/netplan/controller/NetPlanController.java
@@ -31,21 +31,55 @@ public class NetPlanController {
         model.addAttribute("knotInputFormToSave", new KnotInputForm());
         model.addAttribute("operationNumber", knotInputFormList.size()+1);
         model.addAttribute("knotInputFormList", knotInputFormList);
+        model.addAttribute("operation", "eingeben");
         return "knotInputForm";
     }
+
+    @PostMapping("changeInput")
+    public String changeSavedKnotInputForm(Model model, KnotInputForm changeKnotInputForm) {
+        model.addAttribute("knotInputFormToSave", knotInputFormList.get(changeKnotInputForm.getOperationNumber()));
+        model.addAttribute("operationNumber", changeKnotInputForm.getOperationNumber()+1);
+        model.addAttribute("knotInputFormList", knotInputFormList);
+        model.addAttribute("operation", "ändern <a href='http://localhost:8080/input'>|abbrechen|</a>");
+        return "knotInputForm";
+    }
+
+    @GetMapping("deletedOld")
+    public String deleteOldNetPlan () {
+        knotInputFormList = new ArrayList<>();
+        knotList = new ArrayList<>();
+        return "redirect:input";
+    }
+
 
     @PostMapping("saveInput")
     public String saveKnotFormInput(@Valid @ModelAttribute("knotInputFormToSave") KnotInputForm knotInputFormToSave, BindingResult bindingResult, Model model) {
         knotList = new ArrayList<>();
         model.addAttribute("operationNumber", knotInputFormList.size()+1);
         model.addAttribute("knotInputFormList", knotInputFormList);
+        model.addAttribute("operation", "eingeben");
         //validate(knotInputFormToSave, bindingResult);
         System.out.println(bindingResult);
         if (bindingResult.hasErrors()) {
             System.out.println("error");
+            model.addAttribute("knotInputFormToSave", knotInputFormToSave);
             return "knotInputForm";
         }
+
         model.addAttribute("knotInputFormToSave", new KnotInputForm());
+
+        //changing already Input
+        for (KnotInputForm knotInputForm : knotInputFormList) {
+            if (knotInputFormToSave.getOperationNumber() == knotInputForm.getOperationNumber()) {
+                knotInputForm.setOperationDescription(knotInputFormToSave.getOperationDescription());
+                knotInputForm.setOperationNumber(knotInputFormToSave.getOperationNumber());
+                knotInputForm.setDurationInMinutes(knotInputFormToSave.getDurationInMinutes());
+                knotInputForm.setPredecessorOneListIndex(knotInputFormToSave.getPredecessorOneListIndex());
+                knotInputForm.setPredecessorTwoListIndex(knotInputFormToSave.getPredecessorTwoListIndex());
+                knotInputForm.setPredecessorThreeListIndex(knotInputFormToSave.getPredecessorThreeListIndex());
+                return "knotInputForm";
+            }
+        }
 
         System.out.println(knotInputFormToSave);
         knotInputFormToSave.setOperationNumber(knotInputFormList.size()+1);
@@ -54,8 +88,12 @@ public class NetPlanController {
         return "knotInputForm";
     }
 
+
     @GetMapping("netPlanTable")
     public String getNetPlanOutputTable(Model model) {
+        if (knotInputFormList.size() == 0) {
+            return "redirect:input";
+        }
         knotList = convertKnotInputFormListToKnotList(knotInputFormList);
         String error = null;
         error = validateNotTwoEnd(knotList, error);
