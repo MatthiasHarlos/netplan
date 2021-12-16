b0a915746d5205052ab09b4e0c64e43b6f8de3e7
Your Name <you@example.com>
2021-12-16 10:49:25 +0100

    cleaned up test

--- src/test/java/com/newenergytrading/netplan/KnotCalculateLatestTimeTest.java
+++ src/test/java/com/newenergytrading/netplan/KnotCalculateLatestTimeTest.java
@@ -9,36 +9,6 @@ import java.util.List;
 
 public class KnotCalculateLatestTimeTest {
 
-    private List<Knot> knotList(){
-        List<Knot> knotList = new ArrayList<>();
-        knotList.add(new Knot(1,"Entscheidung GE",2,null,null));
-        knotList.add(new Knot(2,"Angebote einholen",14,null,List.of(knotList.get(0))));
-        knotList.add(new Knot(3,"Mitarbeiterinformationen",1,null,List.of(knotList.get(0))));
-        knotList.add(new Knot(4,"Testen G1",1,null,List.of(knotList.get(1))));
-        knotList.add(new Knot(5,"Testen G2",2,null,List.of(knotList.get(1))));
-        knotList.add(new Knot(6,"Testen G3",1,null,List.of(knotList.get(1))));
-        knotList.add(new Knot(7,"Auswahl Lieferanten",1,null,List.of(knotList.get(3), knotList.get(4), knotList.get(5))));
-        knotList.add(new Knot(8,"Lieferung",5,null,List.of(knotList.get(6))));
-        knotList.add(new Knot(9,"Raumauswahl",2,null,List.of(knotList.get(6))));
-        knotList.add(new Knot(10,"Elektroinstallation",2,null,List.of(knotList.get(8))));
-        knotList.add(new Knot(11,"Computeraufstellung",1,null,List.of(knotList.get(7), knotList.get(9))));
-        knotList.add(new Knot(12,"Mitarbeiterschulung",7,null,List.of(knotList.get(2), knotList.get(10))));
-        knotList.add(new Knot(13,"Arbeitsaufnahme",1,null,List.of(knotList.get(11))));
-
-        knotList.get(0).setSuccessor(List.of(knotList.get(1),knotList.get(2)));
-        knotList.get(1).setSuccessor(List.of(knotList.get(3),knotList.get(4), knotList.get(5)));
-        knotList.get(2).setSuccessor(List.of(knotList.get(11)));
-        knotList.get(3).setSuccessor(List.of(knotList.get(6)));
-        knotList.get(4).setSuccessor(List.of(knotList.get(6)));
-        knotList.get(5).setSuccessor(List.of(knotList.get(6)));
-        knotList.get(6).setSuccessor(List.of(knotList.get(7), knotList.get(8)));
-        knotList.get(7).setSuccessor(List.of(knotList.get(10)));
-        knotList.get(8).setSuccessor(List.of(knotList.get(9)));
-        knotList.get(9).setSuccessor(List.of(knotList.get(10)));
-        knotList.get(10).setSuccessor(List.of(knotList.get(11)));
-        knotList.get(11).setSuccessor(List.of(knotList.get(12)));
-        return knotList;
-    }
 
     private List<Knot> resultList(){
         List<Knot> resultList = new ArrayList<>();
